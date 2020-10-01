29ae78d001f0fb07cb5b5fba4ab94beb644f55b4
AndreyKudinov63 <64890665+AndreyKudinov63@users.noreply.github.com>
2020-10-01 15:41:01 +0400

    Updates

--- precache-manifest.41a321269c7f335af449427619b5e2be.js
+++ precache-manifest.41a321269c7f335af449427619b5e2be.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aebbd0c5835bc6f4bc19b6bc02a35161",
+    "revision": "3cd7cba8eb3a8d8b251528f98f1a3527",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "dccad0a7898bb7a10a51",
+    "revision": "26665992cc8afb332b05",
     "url": "/portfolio/static/css/2.b7cef174.chunk.css"
   },
   {
-    "revision": "7252928f467c95fc4fc4",
+    "revision": "f27d2a2e4852915a5ea6",
     "url": "/portfolio/static/css/main.ccfc761a.chunk.css"
   },
   {
-    "revision": "dccad0a7898bb7a10a51",
-    "url": "/portfolio/static/js/2.d5697440.chunk.js"
+    "revision": "26665992cc8afb332b05",
+    "url": "/portfolio/static/js/2.0f204f64.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/portfolio/static/js/2.d5697440.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.0f204f64.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7252928f467c95fc4fc4",
-    "url": "/portfolio/static/js/main.af59ee4f.chunk.js"
+    "revision": "f27d2a2e4852915a5ea6",
+    "url": "/portfolio/static/js/main.a8900788.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2355473ce42121c19026d9ec2d4a0643.js"
+  "/portfolio/precache-manifest.41a321269c7f335af449427619b5e2be.js"
 );
 
 self.addEventListener('message', (event) => {
